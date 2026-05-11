8bfed99504b3d126731d49b562ac280e1e77c7e8
vigneshvaranasi <varanasivsv@gmail.com>
2026-05-11 09:37:17 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">This is studio landing page</h1>
+    </div>
+  )
+}
